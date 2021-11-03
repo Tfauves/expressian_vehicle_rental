62d24c4437fc76424eb1beb4ab73e052b7acf530
Tfauves <tfauvelle@gmail.com>
2021-11-03 14:57:02 -0400

    initial commit

---
+++ src/main/java/com/careerdevs/RESTvehiclerental/RestVehicleRentalApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.RESTvehiclerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVehicleRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVehicleRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/RESTvehiclerental/RestVehicleRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.RESTvehiclerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVehicleRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
