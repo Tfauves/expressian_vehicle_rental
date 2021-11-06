2d7c0356302f788fb202796e35485e3073eea2c7
Tfauves <tfauvelle@gmail.com>
2021-11-05 20:56:54 -0400

    car response entity

--- src/main/java/com/careerdevs/RESTvehiclerental/controllers/CarController.java
+++ src/main/java/com/careerdevs/RESTvehiclerental/controllers/CarController.java
@@ -5,10 +5,19 @@ import com.careerdevs.RESTvehiclerental.models.Car;
 import com.careerdevs.RESTvehiclerental.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+//
+//Create the following routes
+//        Update one by id
+//        destroy one by id
+//        read all by column(field)
+
+
+
 
 @RestController
 @RequestMapping("/api/cars")
@@ -22,14 +31,15 @@ public class CarController {
         return repository.findAll();
     }
 
+
     @GetMapping("/{id}")
     public @ResponseBody Car getOneById(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public Car createCar(@RequestBody Car newCar) {
-        return repository.save(newCar);
+    public ResponseEntity<Car> createCar(@RequestBody Car newCar) {
+        return new ResponseEntity<>(repository.save(newCar), HttpStatus.OK) ;
     }
 
 
