fdb92bb45d4e4f08dbe373b94157b9e558e8e5fe
Tfauves <tfauvelle@gmail.com>
2021-11-08 14:07:09 -0500

    completed mappings.

--- src/main/java/com/careerdevs/RESTvehiclerental/controllers/CustomerController.java
+++ src/main/java/com/careerdevs/RESTvehiclerental/controllers/CustomerController.java
@@ -4,6 +4,7 @@ package com.careerdevs.RESTvehiclerental.controllers;
 import com.careerdevs.RESTvehiclerental.models.Customer;
 import com.careerdevs.RESTvehiclerental.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -48,4 +49,6 @@ public class CustomerController {
     public void deleteCustomer (@PathVariable Long id) {
          repository.deleteById(id);
     }
+
+
 }
--- src/main/java/com/careerdevs/RESTvehiclerental/repositories/CustomerRepository.java
+++ src/main/java/com/careerdevs/RESTvehiclerental/repositories/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.careerdevs.RESTvehiclerental.repositories;
 
 import com.careerdevs.RESTvehiclerental.models.Customer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName, Sort sort);
 }
