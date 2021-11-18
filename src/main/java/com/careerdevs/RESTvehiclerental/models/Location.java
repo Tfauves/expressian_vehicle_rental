0b36d2bcdbd70b579caebb2082c3ac6c5b62f698
Tfauves <tfauvelle@gmail.com>
2021-11-18 10:18:38 -0500

    location entity defined.

---
+++ src/main/java/com/careerdevs/RESTvehiclerental/models/Location.java
@@ -0,0 +1,44 @@
+package com.careerdevs.RESTvehiclerental.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String locationDesignation;
+
+    public Location() {}
+
+    public Location(String locationDesignation) {
+        this.locationDesignation = locationDesignation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocationDesignation() {
+        return locationDesignation;
+    }
+
+    public void setLocationDesignation(String locationDesignation) {
+        this.locationDesignation = locationDesignation;
+    }
+}
