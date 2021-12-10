bc3a403e0ca3a7d35a48d3f2847c82beac8e06c1
Tfauves <tfauvelle@gmail.com>
2021-12-10 16:10:04 -0500

    auth impl.

---
+++ src/main/java/com/careerdevs/RESTvehiclerental/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.careerdevs.RESTvehiclerental.security.services;
+
+import com.careerdevs.RESTvehiclerental.models.auth.User;
+import com.careerdevs.RESTvehiclerental.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Handler;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User Not Found: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
