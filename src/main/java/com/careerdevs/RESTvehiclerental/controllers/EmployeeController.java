aefb87f705d52909add61db3ccc75942364a2fed
Travis J <tfauvelle@gmail.com>
2021-12-25 07:53:14 -0500

    employee mappings.

--- src/main/java/com/careerdevs/RESTvehiclerental/controllers/EmployeeController.java
+++ src/main/java/com/careerdevs/RESTvehiclerental/controllers/EmployeeController.java
@@ -3,7 +3,10 @@ package com.careerdevs.RESTvehiclerental.controllers;
 import com.careerdevs.RESTvehiclerental.models.Employee;
 import com.careerdevs.RESTvehiclerental.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,5 +22,15 @@ public class EmployeeController {
     public @ResponseBody
     List<Employee> getAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Employee getById(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee newEmployee) {
+        return new ResponseEntity<>(repository.save(newEmployee), HttpStatus.CREATED);
     }
 }
