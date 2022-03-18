73666bd311e8cb0686539c661a3b3d8a69040679
Tranquility Base <tfauvelle@gmail.com>
2022-03-17 21:43:20 -0400

    refactor to customer repository

--- src/main/java/com/careerdevs/RESTvehiclerental/repositories/CustomerRepository.java
+++ src/main/java/com/careerdevs/RESTvehiclerental/repositories/CustomerRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    List<Customer> findByLastName(String lastName, Sort sort);
+//    List<Customer> findByLastName(String lastName, Sort sort);
     List<Customer>findAllByRentals_car_id(Long id);
 
 
